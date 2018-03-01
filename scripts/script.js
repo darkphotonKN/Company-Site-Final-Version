1b56cdf1b4febefde0cc64579281911b5bc78325
Kranti Nebhwani <darkphoton20@gmail.com>
2018-03-01 21:26:15 +0800

    nav update

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+
+var navHome = document.getElementById("nav-home");
+var five = 3;
+navHome.addEventListener('mouseover', function() {
+  if('mouseover') {
+    navHome.style.fontSize = '24px';
+  }
+  if('mouseover' == false) {
+    navHome.style.fontSize = '19px';
+  }
+});
