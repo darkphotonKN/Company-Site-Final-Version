fe3253fc4f622092e39d94e6dd19abb5ef1d0eb1
Kranti Nebhwani <darkphoton20@gmail.com>
2018-03-23 18:25:29 +0800

    Sorted JS into functions with init call for clarity. Fixed bugs.

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,36 @@
 
 $(document).ready(function() {
 
-  /* scrolling navigation bar */
+  /* scrolling navigation bar function */
 
-  // variables
+  // global variables
   var navOffsetTop = $('#header-title').offset().top + 10; // find the offset of the nav bar on top
   var scrollPos = 0;
   var headNav = document.getElementById("header-title"); // header title
 
 
   // nav scroll animations when scrolling past nav menu
-  $(window).scroll(function() {
-    scrollPos = $(window).scrollTop(); // value of how much the user has scrolled down the page
-    // console.log(scrollPos);
+  function scrollNav() {
+    $(window).scroll(function() {
+      scrollPos = $(window).scrollTop(); // value of how much the user has scrolled down the page
+      // console.log(scrollPos);
 
-    // add fixed positioning when scrolling past navOffsetTop
-    if(scrollPos >= navOffsetTop) {
-      $('#head-nav').addClass('head-nav');
-      $('#header-title').css('fontSize', '28px');
-      $('#header-title').css('marginTop', '15px');
-      $('#header-title').css('marginBottom', '-20px');
-      $('#header-title').css('color', 'white');
-      //$('.nav').css('color', 'white'); // <- { PROBLEM HERE }
-    } else {
-      $('#head-nav').removeClass('head-nav');
-      $('#header-title').css('fontSize', '27px');
-      $('#header-title').css('marginTop', '0px');
-      $('#header-title').css('marginBottom', '0px');
-      $('#header-title').css('color', '#227093');
-    }
-  });
+      // add fixed positioning when scrolling past navOffsetTop
+      if(scrollPos >= navOffsetTop) {
+        $('#head-nav').addClass('head-nav');
+        $('#header-title').css('fontSize', '28px');
+        $('#header-title').css('marginTop', '15px');
+        $('#header-title').css('marginBottom', '-20px');
+        $('#header-title').css('color', 'white');
+      } else {
+        $('#head-nav').removeClass('head-nav');
+        $('#header-title').css('fontSize', '27px');
+        $('#header-title').css('marginTop', '0px');
+        $('#header-title').css('marginBottom', '0px');
+        $('#header-title').css('color', '#227093');
+      }
+    });
+  }
 
 
   /* responsive mobile nav */
@@ -38,38 +39,48 @@ $(document).ready(function() {
 
   function navAdapt() {
     var windowSize = $window.width();
+    var mynav = document.getElementById("mynav");
+    var mynavMarLeft = window.getComputedStyle(mynav, null).getPropertyValue("margin-left");
+    //var mynavMarLeftCss = "{0}", [mynavMarLeftCss, "stringInject"];
+    //console.log(mynavMarLeftCss);
 
     if(windowSize < mobWidthThres) {
-      console.log('Mobile Mode'); // test
+      //console.log('Mobile Mode'); // test
       $('.mynav').addClass('mobileNavStyle');
       $('#nav-home').addClass('mobileNavStyle');
       $('#mynav').css('marginLeft', '20%');
     } else if(windowSize > mobWidthThres) {
       $('.mynav').removeClass('mobileNavStyle');
       $('#nav-home').removeClass('mobileNavStyle');
-      $('#mynav').css('marginLeft', '60%');
+      $('#mynav').css('marginLeft', '60%'); // sets the original margin back
     }
 
 
   }
-  // check the width on launch and resize nav if width is below threshold
-  navAdapt();
-  // bind this function to an event listener to check for any resizes of the page
-  $window.resize(navAdapt);
 
-
-
-  /* animations */
-
-
-  /* nav */
+  /* other nav animation */
   var navHome = document.getElementById("nav-home");
   navHome.addEventListener('mouseover', function() {
-    navHome.style.marginRight = '5px';
 
   });
 
 
+
+  /* initializes web site javascript functionality */
+  function init() {
+    // check scroll and adjust nav to follow past threshold
+    scrollNav();
+
+    // check the width on launch and resize nav if width is below threshold
+    navAdapt();
+
+    // bind this function to an event listener to check for any resizes of the page
+    $window.resize(navAdapt);
+  }
+
+
+  // launch page
+  init();
 
 
   // tests
